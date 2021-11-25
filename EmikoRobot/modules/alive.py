dbbe4863532c949ee495f208d8819621ac1d9cb7
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-11-25 11:33:53 +0700

    Seth Robot

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,30 +9,30 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
+PHOTO = "https://telegra.ph/file/0943b1675752ff8716d77.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
-  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
-  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
-  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Seth Robot.** \n\n"
+  TEXT += "⚡️**I'm Working Properly** \n\n"
+  TEXT += f"⚡️ **My Master : [ꜱᴇᴛʜ★](https://t.me/xyzseth)** \n\n"
+  TEXT += f"⚡️ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚡️ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚡️ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/EmiexRobot?start=help"), Button.url("Support", "https://t.me/emikosupport")]]
+  BUTTON = [[Button.url("Help", "https://t.me/SethRobot?start=help"), Button.url("Support", "https://t.me/sethproject")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
 
 
 @register(pattern=("/repo"))
 async def source(event):
    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
-   TEXT += "**Here is The source of Emiko Robot** \n\n"
-   TEXT += "**Owner repo : [sena-ex](https://t.me/excrybaby)** \n\n"
+   TEXT += "**Here is The source of Seth Robot** \n\n"
+   TEXT += "**Owner repo : [ꜱᴇᴛʜ★](https://t.me/xyzseth)** \n\n"
    TEXT += f"**Python Version :** `{kontol()}`\n\n"
    TEXT += f"**Library Version :** `{telever}` \n\n"
    TEXT += f"**Telethon Version :** `{tlhver}` \n\n"
    TEXT += f"**Pyrogram Version :** `{pyrover}` \n\n"
-   BUTTON = [[Button.url("Repo", "https://github.com/kennedy-ex/emikorobot"), Button.url("Support", "https://t.me/emikosupport")]]
+   BUTTON = [[Button.url("Repo", "https://github.com/Dorimuhai/sethrobot"), Button.url("Support", "https://t.me/sethproject")]]
    await event.reply(TEXT,  buttons=BUTTON, disable_web_page_preview=True)
 
